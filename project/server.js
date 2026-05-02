c17cb5ee0a4a169eadfa30fb3c2d1746f4ce1c3e
Takshsri <aymar131221@gmail.com>
2026-05-02 22:58:52 +0530

    adding home route

--- project/server.js
+++ project/server.js
@@ -9,8 +9,17 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.json({
+    message: "School Management API is running 🚀",
+    endpoints: {
+      addSchool: "POST /api/schools/add",
+      listSchools: "GET /api/schools/listSchools?latitude=17.385&longitude=78.4867"
+    }
+  });
+});
 app.use('/api/schools',schoolRoutes);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
